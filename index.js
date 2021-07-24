c8c3673c77a5985370909a32ca67891958170fd6
David <dslusser13@gmail.com>
2021-07-24 16:55:02 +0000

    added https support

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const path = require("path");
 const logger = require("./middleware/logger");
 const cors = require("cors");
+const https = require("https");
+const fs = require("fs");
 
 const app = express();
 
@@ -22,4 +24,12 @@ app.use("/api/events", require("./routes/api/events"));
 
 const PORT = process.env.PORT || 5001;
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+//app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+
+//listen on both http and https
+const httpsServer = https.createServer({
+	key: fs.readFileSync('/etc/letsencrypt/live/dslusser.com/privkey.pem'),
+	cert: fs.readFileSync('/etc/letsencrypt/live/dslusser.com/fullchain.pem'),
+}, app);
+
+httpsServer.listen(5001, "0.0.0.0");
